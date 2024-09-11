17ca52f62c3861c734b41f65618a91ae220e3351
seifgendy <141524686+seifgendy@users.noreply.github.com>
2024-09-11 17:20:35 +0300

    Add files via upload

---
+++ amit cots/main.c
@@ -0,0 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Aug 25, 2023
+ *      Author: A&G Computer World
+ */
+
+#include <avr/io.h>
+int main (){
+
+	while(1){
+DDRD = 10 ;
+
+
+
+	}
+	return 0;
+
+
+}
